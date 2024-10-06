bb67bd4fdd2c8c610ece298317cf628d8c8461f7
Harry Chen <czy88840616@gmail.com>
2024-10-06 17:56:37 +0800

    chore: up

---
+++ packages/case-node16-interop/src/index.ts
@@ -0,0 +1,7 @@
+import jsExports from '@my-workspace/js-exports';
+import jsGlobalExports from '@my-workspace/js-global-exports';
+
+console.log('Imported from js-exports:', jsExports);
+console.log('Imported from js-global-exports:', jsGlobalExports);
+
+// 这里我们不导出任何内容,只是演示导入
